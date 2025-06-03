622c2112e1e6592fac6cb7268165c10bb38b09f9
mmaheen <mushrifmaheen@gmail.com>
2025-06-03 18:20:08 +0600

    Landing page

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <RouterLink class="navbar-brand" to="/">
+        <i class="fas fa-film mr-2"></i>
+        Gallery
+      </RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <RouterLink class="nav-link nav-link-1 active" aria-current="page" to="/">
+              Photos
+            </RouterLink>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link nav-link-2" href="videos.html">Videos</a>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link nav-link-3" to="/about">About</RouterLink>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link nav-link-4" href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'\
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+import Search from '../components/Search.vue'
+import Photos from '../components/Photos.vue'
 </script>
 
-<template></template>
+<template>
+  <Header />
+  <Search />
+  <Photos />
+  <Footer />
+</template>
